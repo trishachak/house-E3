6eedfa478c8e7f12b83a1735836bfce00c3ab0fc
trisha <unipegachicken27@gmail.com>
2023-06-26 13:59:33 -0400

    Initial Commit

---
+++ house E3/house_E3App.swift
@@ -0,0 +1,17 @@
+//
+//  house_E3App.swift
+//  house E3
+//
+//  Created by Trisha Chakraborty on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct house_E3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
